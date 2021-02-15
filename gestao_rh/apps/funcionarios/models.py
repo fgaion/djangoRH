cc65048dbee643e3514eb99a5e050ef945755079
Fabio Gaion <fabiogaion@gmail.com>
2021-02-15 19:25:13 -0300

    Criando os relacionamentos do BD

--- gestao_rh/apps/funcionarios/models.py
+++ gestao_rh/apps/funcionarios/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-#from django.contrib.auth.models import User
-#from gestao_rh.apps.departamentos.models import Departamento
-#from gestao_rh.apps.empresas.models import Empresa
+from django.contrib.auth.models import User
+from gestao_rh.apps.departamentos.models import Departamento
+from gestao_rh.apps.empresas.models import Empresa
 
 
 # Create your models here.
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
-    #user = models.OneToOneField(User, on_delete=models.PROTECT)
-    #departamentos = models.ManyToManyField(Departamento)
-    #empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.nome
