23d039530e6f0dbc056e44eb59f20fa7783e347d
Fabio Gaion <fabiogaion@gmail.com>
2021-02-27 14:56:21 -0300

    adcionando IP publico

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -24,7 +24,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','3.138.188.159']
 
 
 # Application definition
