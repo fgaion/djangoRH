f684baaa69962420b346a0f3ca789246599a95d3
Fabio Gaion <fabiogaion@gmail.com>
2021-03-13 16:44:14 -0300

    Instalando o celery/redis

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'gestao_rh.apps.documentos',
     'gestao_rh.apps.registro_hora_extra',
     'gestao_rh.apps.core',
-    'bootstrapform'
+    'bootstrapform',
+    'django_celery_results'
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
